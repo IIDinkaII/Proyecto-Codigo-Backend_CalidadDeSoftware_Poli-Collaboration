c317ba50dd3d3bf583958c549b8726282a1e9ad6
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-07-23 13:20:46 -0500

    refactor: change file name

---
+++ poli-collaboration-api/src/usuarios/dtos/usuario.dto.ts
@@ -0,0 +1,52 @@
+import { EstadoUsuario } from '../entities/estado-usuario.entity';
+import { PerfilUsuario } from '../entities/pefil-usuario.entity';
+import { UsuarioRol } from '../../entities/usuario-rol.entity';
+import { Publicacion } from '../../publicaciones/entities/publicacion.entity';
+import { Respuesta } from '../../publicaciones/preguntas/entities/respuesta.entity';
+import { IsString, IsNumber, IsDate, IsNotEmpty } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CrearUsuarioDto {
+  @IsNumber()
+  @IsNotEmpty()
+  readonly id_usuario: number;
+  @IsNotEmpty()
+  readonly estado: EstadoUsuario;
+  @IsNotEmpty()
+  readonly perfil: PerfilUsuario;
+  @IsNotEmpty()
+  readonly usuariosRol: UsuarioRol[];
+  @IsNotEmpty()
+  readonly publicaciones: Publicacion[];
+  @IsNotEmpty()
+  readonly respuestas: Respuesta[];
+  @IsString()
+  @IsNotEmpty()
+  readonly correo_institucional: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly nombres: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly apellidos: string;
+  @IsDate()
+  @IsNotEmpty()
+  readonly fecha_nacimiento: Date;
+  @IsString()
+  @IsNotEmpty()
+  readonly carrera: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly facultad: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+  @IsDate()
+  @IsNotEmpty()
+  readonly fecha_creacion: Date;
+  @IsDate()
+  @IsNotEmpty()
+  readonly ultima_actualizacion: Date;
+}
+
+export class ActualizarUsuarioDto extends PartialType(CrearUsuarioDto) {}

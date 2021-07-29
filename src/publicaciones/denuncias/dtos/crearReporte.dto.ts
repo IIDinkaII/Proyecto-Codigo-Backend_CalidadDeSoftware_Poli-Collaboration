24ca7e0b1fb166182ca225f01f02ee3c5eef6974
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2021-07-28 23:04:13 -0500

    feat: create dto crearReporte

---
+++ src/publicaciones/denuncias/dtos/crearReporte.dto.ts
@@ -0,0 +1,30 @@
+import {
+    IsString,
+    IsDate,
+    IsNotEmpty,
+  } from 'class-validator';
+  import { ApiProperty } from '@nestjs/swagger';
+  
+  export class CrearReporteDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly accionesTomadas: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly responsables: string;
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly evidencia: string;
+    @IsDate()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly fechaCreacion: Date;
+    @IsDate()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly ultimaActualizacion: Date;
+  }
+  

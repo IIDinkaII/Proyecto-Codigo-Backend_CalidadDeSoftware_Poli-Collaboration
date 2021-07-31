4298d1e9349113c0f8ffe31ec56ee65a272ec532
AndersCFR <anderson.cardenas.r@gmail.com>
2021-07-31 17:33:08 -0500

    feat: revision controller

--- src/publicaciones/revisiones/controllers/revision.controller.ts
+++ src/publicaciones/revisiones/controllers/revision.controller.ts
@@ -17,6 +17,7 @@ import {
   import { RolesGuard } from 'src/auth/guards/roles.guard';
   import { RevisionService } from '../services/revision.service'; 
   import { DenunciaService } from 'src/publicaciones/denuncias/services/denuncia.service';
+import { CrearRevisionDTO, ActualizarRevisionDTO } from '../dtos/revision.dto';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('revision')
@@ -24,7 +25,33 @@ import {
 export class RevisionController {
     constructor(
         private _httpRevisionService: RevisionService,
-        private _httpDenunciaService: DenunciaService,
+        //private _httpDenunciaService: DenunciaService,
     ) {}
 
+    @Roles(Role.MODERADOR)
+    @Get()
+    @ApiOperation({ summary: 'Lista de revisiones de la aplicación.' })
+    getAll() {
+        return this._httpRevisionService.findAll();
+      }
+  
+    @Get(':id')
+    @ApiOperation({ summary: 'Obtener una revision según su Id' })
+    getOne(@Param('id', ParseIntPipe) id: number) {
+      return this._httpRevisionService.findOne(id);
+    }
+  
+    @Public()
+    @Post()
+    @ApiOperation({ summary: 'Crear una revision' })
+    async create(@Body() denuncia: CrearRevisionDTO) {
+      return this._httpRevisionService.create(denuncia);
+    }
+  
+    @Put(':id')
+    @ApiOperation({ summary: 'Actualizar una revision' })
+    update(@Param('id') id: number, @Body() body: ActualizarRevisionDTO) {
+      return this._httpRevisionService.update(id, body);
+    }
+  
 }

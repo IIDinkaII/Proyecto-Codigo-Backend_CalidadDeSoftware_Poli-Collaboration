b18cf873f043afbdf35d4b6746e495e441cc138e
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-07-31 10:36:35 -0500

    fixed to heroku

--- src/publicaciones/denuncias/denuncias.module.ts
+++ src/publicaciones/denuncias/denuncias.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioService } from 'src/usuarios/services/usuario.service';
 import { DenunciaController } from './controllers/denuncia.controller';
 import { Denuncia } from './entities/denuncia.entity';
 import { DenunciaService } from './services/denuncia.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Denuncia])],
+  imports: [TypeOrmModule.forFeature([Denuncia]), UsuarioService],
   controllers: [DenunciaController],
   providers: [DenunciaService],
 })

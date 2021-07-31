86e495d7c4da0d2f8070892f0a2f9fcba5852222
AndersCFR <anderson.cardenas.r@gmail.com>
2021-07-31 13:27:07 -0500

    feat: revision DTO

--- src/publicaciones/revisiones/dtos/revision.dto.ts
+++ src/publicaciones/revisiones/dtos/revision.dto.ts
@@ -8,43 +8,16 @@ import{
 import { ApiProperty } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/swagger';
 
-export class CrearDenunciaDTO {
+export class CrearRevisionDTO {
     @IsNumber()
     @IsNotEmpty()
     @ApiProperty()
-    readonly idUsuario: number;
+    readonly idDenuncia: number;
     @IsString()
     @IsNotEmpty()
     @ApiProperty()
-    readonly modoCanal: string;
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    readonly telefonoContacto: string;
-    @IsOptional()
-    @IsNotEmpty()
-    @ApiProperty()
-    readonly estado: string;
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    readonly tipoDenuncia: string;
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    readonly descripcionHechos: string;
-    @IsOptional()
-    @ApiProperty()
-    readonly adjunto: string;
+    readonly observacion: string;
+ 
 }
 
-export class ActualizarDenunciaDTO extends PartialType(CrearDenunciaDTO) {}
-
-export class ActualizarEstadoDenunciaDTO{
-    @IsOptional()
-    @IsNotEmpty()
-    @ApiProperty()
-    readonly estado: tiposEstado;
-}
-
-type tiposEstado = "En revisión"|"Creado"|"Finalizado"
+export class ActualizarDenunciaDTO extends PartialType(CrearRevisionDTO) {}
